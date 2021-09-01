2cd27f12adae7c034e17a61bd0a0fa430f605cd6
iosexperts <iosexperts@rvtechnologies.com>
2021-09-01 15:02:15 +0530

    intial commit

---
+++ App/Containers/Styles/GrnHomeStyle.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+import { Colors, Metrics } from '../../Themes/'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  backgroundImage: {
+        width: '100%',
+        height: '100%'
+    },
+  inoappsLogo: {
+    width: '30%',
+    height: '30%'
+  },
+  logoContainer: {
+    flex: 0.07,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent'
+  }
+})
